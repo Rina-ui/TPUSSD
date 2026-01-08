12dda56c6221cbf9c3f826199eb00c4be87325fa
Rina-ui <marinagracia895@gmail.com>
2026-01-08 16:18:31 +0000

    modifictaion

---
+++ lib/data/datasource/remote/ussd_service.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class UssdService extends StatefulWidget {
+  const UssdService({super.key});
+
+  @override
+  State<UssdService> createState() => _UssdServiceState();
+}
+
+class _UssdServiceState extends State<UssdService> {
+
+  TextEditingController _numberCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
